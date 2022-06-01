e9cf9978ebfdc9d4b0859b6db017ac20b64c1b2c
Ceci <ceciliaagarzon@yahoo.com>
2022-06-01 12:34:38 -0500

    login and gitignore
    Updating the login file and adding the gitignore file.

--- admin/login.php
+++ admin/login.php
@@ -11,18 +11,19 @@ include("../basics/footer.php");
 
         <div class="card">
             <div class="card-header">
-                Welcome admin, please enter data to login
+                <h2>Welcome admin!</h2>
+                <p>Please enter data to login</p>
             </div>
             <div class="card-body">
                 <blockquote class="blockquote mb-0">
                     <form>
                         <div class="form-group">
                             <label for="username">Username</label>
-                            <input type="text" class="form-control" name="username" id="username" placeholder="Enter username">                            
+                            <input type="text" class="form-control" name="username" id="username" placeholder="Enter username" required>                            
                         </div>
                         <div class="form-group">
                             <label for="password">Password</label>
-                            <input type="password" class="form-control" name="password" id="password" placeholder="Enter password">
+                            <input type="password" class="form-control" name="password" id="password" placeholder="Enter password" required>
                         </div>
                         <hr>                        
                         <button type="submit" class="btn btn-info">Login</button>

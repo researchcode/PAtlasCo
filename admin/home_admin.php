57e04193984f74950ee66eb7c0dddc67f6701545
Ceci <ceciliaagarzon@yahoo.com>
2022-06-02 18:37:09 -0500

    Admin options
    Admin options are being configured to insert basic data such as the entity, info about the country in which the map will focus, and a table which will to show the list of markers to set in the map. These markers will be added by the user.

--- admin/home_admin.php
+++ admin/home_admin.php
@@ -3,4 +3,88 @@ session_start();
 unset($_SESSION['user_user']);
 $_SESSION['admin_admin'] = 1;
 
+include("../basics/header.php");
+?>
+<!--Main layout-->
+<main class="mt-5">
+    <div class="col-12">
+        <section id="">
+            <div class="row d-flex justify-content-center">
+                <div class="card p-s " style="width: 25rem;">
+                    <div class="card-body">
+                        <form action="">
+                            <h5 class="card-title">Entity</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">A word that represents a location-based entity (i.e. parks)</h6>
+                            <div class="form-group">
+                                <label for="">Entity</label>
+                                <input type="text" name="entity" class="form-control">
+                            </div>
+                            <hr>
+                            <button type="submit" class="btn btn-info">Save</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="card p-2 offset-md-1" style="width: 25rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Country</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Give the coordinates of the country to focus the map on that country </h6>
+                        <form action="">
+                            <div class="form-group">
+                                <label for="">Latitude</label>
+                                <input type="text" class="form-control">
+                                <small id="emailHelp" class="form-text text-muted">i.e. 4.40</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Longitude</label>
+                                <input type="text" class="form-control">
+                                <small id="emailHelp" class="form-text text-muted">i.e. -72.9301367</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Zoom</label>
+                                <input type="text" class="form-control">
+                                <small id="emailHelp" class="form-text text-muted">A number from 0.0 to 18.0</small>
+                            </div>
+                            <hr>
+                            <button type="submit" class="btn btn-info">Save</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <hr>
+        <section id="">
+            <div class="row d-flex justify-content-center">
+                <div class="card" style="width: 80rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">List of markers</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Each marker is set as a point on the map</h6>
+                        <button class="btn btn-info">New</button>
+                        <hr>
+                        <table class="table table-striped table-info">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Latitude</th>
+                                    <th scope="col">Longitude</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>                               
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+</main>
+<?php
+include("../basics/footer.php");
 ?>
--- model/functions.php
+++ model/functions.php
@@ -1,15 +1,26 @@
 <?php
 
-class Functions{
-    public $con = Conexion::getConnection();
-    function validateLogin(){
-        
-        $queryViaje = "SELECT * FROM users WHERE username=" . $_POST['username'] . "AND password=" . $_POST['password'];
-        $result = $this->con->query($queryViaje);
+require("conexion.php");
+
+if (isset($_POST["login"])) {
+    if (validateLogin() != null) {
+        header('Location:../admin/home_admin.php?info=1');
+    } else {
+        header('Location:../admin/login.php?info=1');
+    }
+}
+
+function validateLogin()
+{
+    $con = Conexion::getConnection();
+    $queryUser = "SELECT * FROM users WHERE username='" . $_POST['username'] . "' AND password='" . $_POST['password'] ."'";    
+    $result = $con->query($queryUser);
+    if ($result != null) {
         if ($result->num_rows > 0) {
+            print_r($result);
             return $result;
         }
+    } else {
         return null;
     }
 }
-?>
